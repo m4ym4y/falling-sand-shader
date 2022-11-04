c9bdb8b1afeb12ae733ea910710e523e01c270a3
m4ym4y <m4ym4y@users.noreply.github.com>
2022-11-04 13:31:34 -0700

    add simple fire

--- index.js
+++ index.js
@@ -32,59 +32,92 @@ const vertex_shader = `
   }
 `
 
+const sand = [ 246, 215, 176, 255 ]
+const wall = [ 127, 127, 127, 255 ]
+const empty = [ 0, 0, 0, 255 ]
+const fire = [ 255, 0, 0, 255 ]
+const fireB1 = [ 204, 0, 0, 255 ]
+const fireB2 = [ 153, 0, 0, 255 ]
+
 // TODO: get the passed in scale to work
 const fragment_shader = `
-  precision mediump float;
+  precision highp float;
   uniform sampler2D state;
   // uniform vec2 scale;
+  uniform highp float seed;
   #define scale vec2(1024.0, 1024.0)
 
-  int get(int x, int y) {
-    return int(texture2D(state, (gl_FragCoord.xy + vec2(x, y)) / scale).r);
-  }
+  // 246,215,176
+  #define sand vec4(0.9647058823529412, 0.8431372549019608, 0.6901960784313725, 1.0)
+  // 127,127,127
+  #define wall vec4(0.4980392156862745, 0.4980392156862745, 0.4980392156862745, 1.0)
+  // 0,0,0
+  #define empty vec4(0.0, 0.0, 0.0, 1.0)
 
-  int getborder(int x, int y) {
-    return int(texture2D(state, (gl_FragCoord.xy + vec2(x, y)) / scale).g);
+  // 255, 0, 0
+  #define fire vec4(1.0, 0.0, 0.0, 1.0)
+  // 204, 0, 0
+  #define fire_burnout_1 vec4(0.8, 0.0, 0.0, 1.0)
+  // 153, 0, 0
+  #define fire_burnout_2 vec4(0.6, 0.0, 0.0, 1.0)
+
+  vec4 get(int x, int y) {
+    return vec4(texture2D(state, (gl_FragCoord.xy + vec2(x, y)) / scale));
+  }
+  
+  float rand(vec2 co){
+    return fract(sin(dot(co, vec2(12.9898, 78.233))) * 43758.5453);
   }
 
   void main() {
-    bool iswall = getborder(0, 0) > 0;
+    vec4 current = vec4(get(0, 0));
+    bool iswall = current == wall;
+    bool sandontop = get(0, 1) == sand;
+    bool onbottom = get(0, -1) == sand || get(0, -1) == wall;
+    bool issand = current == sand;
 
-    bool particleontop = get(0, 1) > 0;
-    bool wallonbottom = getborder(0, -1) > 0;
-    bool particleonbottom = get(0, -1) > 0;
-    bool onbottom = wallonbottom || particleonbottom;
-    bool filled = get(0, 0) > 0;
-
-    /*if (scale.x == 1024.0) {
-      gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
-    } else if (scale.x > 1024.0) {
-      gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-    } else if (scale.x == 0.0 && scale.y == 0.0) {
-      gl_FragColor = vec4(0.0, 1.0, 1.0, 1.0);
-    }*/
+    bool isfireadjacent =
+      get(0, -1) == fire ||
+      get(1, -1) == fire ||
+      get(-1, -1) == fire ||
+      get(1, 0) == fire ||
+      get(-1, 0) == fire ||
+      get(1, 1) == fire ||
+      get(-1, 1) == fire;
 
     if (iswall) {
-      gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
+      gl_FragColor = wall;
     } else {
-      // particle lands on something = filled
-      if (filled && onbottom) {
-        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-      }
+      if (issand) {
+        // particle exposed to fire = fire
+        if (isfireadjacent && rand(vec2(gl_FragCoord) * seed) > 0.5) {
+          gl_FragColor = fire;
+        // particle lands on something = filled
+        } else if (onbottom) {
+          gl_FragColor = sand;
 
+        // particle is falling out of this cell, nothing falling in = empty
+        } else {
+          gl_FragColor = empty;
+        }
+      }
+      
       // particle is falling into this cell = filled
-      else if (!filled && particleontop) {
-        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+      else if (sandontop) {
+        gl_FragColor = sand;
       }
 
-      // particle is falling out of this cell, nothing falling in = empty
-      else if (filled && !onbottom) {
-        gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+      else if (current == fire) {
+        gl_FragColor = fire_burnout_1;
+      } else if (current == fire_burnout_1) {
+        gl_FragColor = fire_burnout_2;
+      } else if (current == fire_burnout_2) {
+        gl_FragColor = empty;
       }
 
       // not filled, nothing on bottom
       else {
-        gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+        gl_FragColor = empty;
       }
     }
   }
@@ -171,18 +204,21 @@ function step (gl, stepBuffer, frontTexture, backTexture, sizeX, sizeY, program)
   const scaleV = new Float32Array([ sizeX, sizeY ])
   gl.uniform2fv(scale, scaleV)
 
+  const seed = gl.getUniformLocation(program, 'seed')
+  gl.uniform1f(seed, Math.random())
+
   drawQuad(gl, program)
 }
 
-function getRandomCells (sizeX, sizeY, concentration) {
+function getRandomCells (sizeX, sizeY, concentration, color) {
   const source = new Uint8Array(sizeX * sizeY * 4)
   for (let i = 0; i < sizeX * sizeY; ++i) {
     const pixi = i * 4
     const isAlive = Math.random() < concentration
-    source[pixi] = isAlive ? 255 : 0
-    source[pixi + 1] = 0
-    source[pixi + 2] = 0
-    source[pixi + 3] = 255
+    const cellColor = isAlive ? color : [ 0, 0, 0, 255 ]
+    for (let component = 0; component < 4; ++component) {
+      source[pixi + component] = cellColor[component]
+    }
   }
 
   return source
@@ -202,8 +238,8 @@ function setBorder (source, sizeX, sizeY, color) {
 }
 
 function setRandom (gl, texture, sizeX, sizeY, concentration) {
-  const source = getRandomCells(sizeX, sizeY, concentration)
-  setBorder(source, sizeX, sizeY, [0, 255, 0, 255])
+  const source = getRandomCells(sizeX, sizeY, concentration, sand)
+  setBorder(source, sizeX, sizeY, wall)
 
   gl.bindTexture(gl.TEXTURE_2D, texture)
   gl.texSubImage2D(gl.TEXTURE_2D, 0, 0, 0, sizeX, sizeY, gl.RGBA, gl.UNSIGNED_BYTE, source)
@@ -273,15 +309,19 @@ async function main () {
   const brushes = [
     {
       name: 'sand',
-      color: [255, 0, 0, 255]
+      color: sand
     },
     {
       name: 'wall',
-      color: [0, 255, 0, 255]
+      color: wall
+    },
+    {
+      name: 'fire',
+      color: fire
     },
     {
       name: 'erase',
-      color: [0, 0, 0, 255]
+      color: empty
     }
   ]
   let brushType = 0
@@ -327,7 +367,7 @@ async function main () {
 
     drawState(gl, sizeX, sizeY, frontTexture, copyProgram)
     frames++
-    await new Promise(resolve => setTimeout(resolve, 8))
+    await new Promise(resolve => setTimeout(resolve, 0))
   }
 }
 
